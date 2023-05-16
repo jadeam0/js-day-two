aaff40e2236fdee75d5d269b0f28c8040cc3003f
jadeam0 <jdpaulse27@gmail.com>
2023-05-16 16:24:25 +0200

    first

---
+++ code.js
@@ -0,0 +1,11 @@
+let subject= ["Python", "C++","VB.Net", "Javescript", "Assembly"];
+
+//last elm
+// const lastsubject = subject[subject.length -1];
+// console.log (lastsubject);
+
+let lastElement = subject.pop();
+console.log(lastElement);
+
+//middle elm
+console.log (subject [2]);
